f2c9b7d62ca6dc187f0ebe8a65cc6660a8e8679d
Smijith B <nambiarsmith@gmail.com>
2017-03-05 19:23:22 +0530

    Updated all nuget packages.

---
+++ JaiSmi/App_Start/UserMembership.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JaiSmi.App_Start
+{
+    public class UserMembership
+    {
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+
+        public static void Login()
+        {
+            
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
