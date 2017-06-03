2b52f63e992d69cdf8685745d3cdacd8bd78e342
Smijith B <nambiarsmith@gmail.com>
2017-06-03 14:32:18 +0530

    Added user model and account helper.

---
+++ JaiSmi/DAL/Helpers/AccountHelper.cs
@@ -0,0 +1,47 @@
+﻿using JaiSmi.DAL.EntityModel;
+using JaiSmi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JaiSmi.DAL.Helpers
+{
+    public class AccountHelper
+    {
+        #region Public Methods
+
+        public UserModel GetUser(string username, string password)
+        {
+            using (var context = new SJEntities())
+            {
+                return (from u in context.USERS
+                        select new UserModel
+                        {
+                            Id = u.USER_ID,
+                            FirstName = u.FIRST_NAME,
+                            LastName = u.LAST_NAME,
+                            Avatar = u.PROFILE_PIC,
+                            Description = u.DESCRIPTION,
+                            Url = u.URL
+                        }).FirstOrDefault();
+            }
+        }
+
+        public bool CreateUser(UserModel user)
+        {
+            bool isUserCreated = false;
+
+            return isUserCreated;
+        }
+
+        
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
---
+++ JaiSmi/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JaiSmi.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Avatar { get; set; }
+    }
+}
