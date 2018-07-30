2fdf561842c9599803c873d8cbe7ed1996623e1a
Smijith B <nambiarsmith@gmail.com>
2018-07-30 14:50:39 +0530

    Added owin for implementing external auth provider

---
+++ JaiSmi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JaiSmi.Startup))]
+namespace JaiSmi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
